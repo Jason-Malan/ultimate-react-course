7e5304f9ffcd716ad0ec6adaa521ddca3477f22b
Jason Malan <jasondm1291@gmail.com>
2023-09-17 21:14:24 +0200

    Finished static html for eat n split.

---
+++ 06-eat-n-split/eat-n-split/src/App.js
@@ -0,0 +1,116 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendList />
+        <Button>Add friend</Button>
+        <FormAddFriend />
+      </div>
+
+      <FormSplitBill />
+    </div>
+  );
+}
+
+function FriendList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((f) => (
+        <Friend friend={f} key={f.id}></Friend>
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you {Math.abs(friend.balance)}$
+        </p>
+      )}
+
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function FormAddFriend() {
+  return (
+    <form className="form-add-friend">
+      <label htmlFor="">🤼 Friend name</label>
+      <input type="text" />
+
+      <label htmlFor="">📸 Image URL</label>
+      <input type="text" />
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+
+      <label>💰 Bill value</label>
+      <input type="text" />
+
+      <label>👨‍🎤 Your expense</label>
+      <input type="text" />
+
+      <label>🤼 X's expense</label>
+      <input type="text" />
+
+      <label>🤑 Who is paying</label>
+      <select>
+        <option value="user">You</option>
+        <option value="friend">X</option>
+      </select>
+
+      <Button>Split bill</Button>
+    </form>
+  );
+}
+
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
