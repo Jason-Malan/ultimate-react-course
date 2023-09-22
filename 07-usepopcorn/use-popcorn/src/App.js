2f605eb4ffe6d888149c79d688e6926261c6875f
Jason Malan <jason.malan@digital2dna.com>
2023-09-22 08:20:07 +0200

    Default markup and styling for movie details.

--- 07-usepopcorn/use-popcorn/src/App.js
+++ 07-usepopcorn/use-popcorn/src/App.js
@@ -174,12 +174,58 @@ function Movie({ movie, onSelectMovie }) {
 }
 
 function MovieDetails({ selectedId, onCloseMovie }) {
+  const [movie, setMovie] = useState({});
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(() => {
+    async function getMovieDetails() {
+      const res = await fetch(`${OMDB_URL}i=${selectedId}`);
+      const data = await res.json();
+      setMovie(data);
+    }
+
+    getMovieDetails();
+  }, []);
+
   return (
-    <div className="detail">
-      <div className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </div>
-      {selectedId}
+    <div className="details">
+      <header>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+        <img src={poster} alt={`Poster of ${movie} movie`} />
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>
+            {released} &bull; {runtime}
+          </p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐️</span>
+            {imdbRating} IMDb rating
+          </p>
+        </div>
+      </header>
+
+      <section>
+        <p>
+          <em>{plot}</em>
+        </p>
+        <p>Starring {actors}</p>
+        <p>Directed by {director}</p>
+      </section>
     </div>
   );
 }
