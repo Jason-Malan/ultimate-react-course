6c56b430b6277e13d238062b55a22bb61e1c102d
Jason Malan <jason.malan@digital2dna.com>
2023-09-25 18:10:08 +0200

    Added react router.

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -1,6 +1,18 @@
-function App() {
-  const a = "";
-  return <div>WorldWise</div>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
