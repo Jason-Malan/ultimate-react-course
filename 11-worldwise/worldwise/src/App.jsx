d850c36203b898c9b7d4ac1a310f62ba2e709bd9
Jason Malan <jason.malan@digital2dna.com>
2023-09-26 21:13:32 +0200

    Added city list.

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -5,8 +5,32 @@ import Home from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import { useEffect, useState } from "react";
+import CityList from "./components/CityList";
+
+const BASE_URL = "http://localhost:9000";
 
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error loading the cities");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <>
       <BrowserRouter>
@@ -16,8 +40,14 @@ export default function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>Cities</p>}></Route>
-            <Route path="cities" element={<p>Cities</p>}></Route>
+            <Route
+              index
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            ></Route>
+            <Route
+              path="cities"
+              element={<CityList cities={cities} isLoading={isLoading} />}
+            ></Route>
             <Route path="countries" element={<p>Countries</p>}></Route>
             <Route path="form" element={<p>Form</p>}></Route>
           </Route>
