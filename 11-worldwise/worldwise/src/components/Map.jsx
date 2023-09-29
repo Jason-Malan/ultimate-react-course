0da0877a0b0be8817e60948995b3e7049b54e607
Jason Malan <jason.malan@digital2dna.com>
2023-09-29 21:54:40 +0200

    Using search params in url.

--- 11-worldwise/worldwise/src/components/Map.jsx
+++ 11-worldwise/worldwise/src/components/Map.jsx
@@ -1,5 +1,20 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 export default function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        Position: {lat},{lng}
+      </h1>
+      <button onClick={() => setSearchParams({ lat: 50, lng: 25 })}>
+        Set Position
+      </button>
+    </div>
+  );
 }
