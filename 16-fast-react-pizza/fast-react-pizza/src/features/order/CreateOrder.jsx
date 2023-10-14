92b63db341170530ba292a5d9e167490b5ad329f
Jason Malan <jason.malan@digital2dna.com>
2023-10-14 21:05:57 +0200

    Using the apply for classes.

--- 16-fast-react-pizza/fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 16-fast-react-pizza/fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -45,13 +45,13 @@ function CreateOrder() {
       <h2>Ready to order? Let's go!</h2>
       <div>
         <label>First Name</label>
-        <input type="text" name="customer" required />
+        <input type="text" name="customer" required className="input" />
       </div>
 
       <div>
         <label>Phone number</label>
         <div>
-          <input type="tel" name="phone" required />
+          <input type="tel" name="phone" required className="input" />
         </div>
         {formErrors?.phone && <p>{formErrors.phone}</p>}
       </div>
@@ -59,13 +59,7 @@ function CreateOrder() {
       <div>
         <label>Address</label>
         <div>
-          <input
-            type="text"
-            name="address"
-            required
-            className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm outline-none transition-all duration-100
-            placeholder:text-stone-300 focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
-          />
+          <input type="text" name="address" required className="input" />
         </div>
       </div>
 
