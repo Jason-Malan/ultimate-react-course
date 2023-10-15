cc37fcdaf3dc36fd8a9f6c078d2f212c8d7e9e67
Jason Malan <jasondm1291@gmail.com>
2023-10-15 19:54:51 +0200

    Modelling the cart state.

---
+++ 16-fast-react-pizza/fast-react-pizza/src/features/cart/cartSlice.js
@@ -0,0 +1,50 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const initialState = {
+  cart: [],
+  cart: [
+    {
+      pizzaId: 12,
+      name: 'Madndyasda',
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addItem(state, action) {
+      state.cart.push(action.payload);
+    },
+    deleteItem(state, action) {
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
+    },
+    increaseItemQty(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    decreaseItemQty(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--;
+      item.totalPrice = item.quantity * item.unitPrice;
+    },
+    clearCart(state, action) {
+      state.cart = [];
+    },
+  },
+});
+
+export const {
+  addItem,
+  clearCart,
+  decreaseItemQty,
+  deleteItem,
+  increaseItemQty,
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
