05bae09eb1b043f26dcd876dcfd592c42fb731e3
Jason Malan <jasondm1291@gmail.com>
2023-10-22 13:48:33 +0200

    Styling components with css function and as prop.

--- 17-the-wild-oasis/the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/the-wild-oasis/src/App.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: yellow;
-`;
+import Heading from "./ui/Heading";
 
 const StyledApp = styled.div`
   background-color: orangered;
@@ -19,9 +14,11 @@ export default function App() {
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Hello world</H1>
+        <Heading as="h1">The wild oasis</Heading>
+        <Heading as="h2">Check in and out</Heading>
         <Button>Check in</Button>
         <Button>Check out</Button>
+        <Heading as="h3">Form</Heading>
       </StyledApp>
     </>
   );
