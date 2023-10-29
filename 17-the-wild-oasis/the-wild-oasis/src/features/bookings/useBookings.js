83d6381ff32c1948f15c4c87aa739595c1066827
Jason Malan <jasondm1291@gmail.com>
2023-10-29 16:54:26 +0200

    Adding pre fetch for bookings.

--- 17-the-wild-oasis/the-wild-oasis/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,9 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 function useBookings() {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   // FILTER
   const filterValue = searchParams.get("status");
@@ -27,8 +29,23 @@ function useBookings() {
   } = useQuery({
     queryKey: ["bookings", filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
-    keepPreviousData: false,
   });
+
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  // PRE-FETCHING
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  // PRE-FETCHING
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { isLoading, bookings, count, error };
 }
