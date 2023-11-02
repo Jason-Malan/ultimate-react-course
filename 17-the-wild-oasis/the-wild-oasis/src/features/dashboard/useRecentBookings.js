c5eb557b82f7a3448e78c8836efbb5e35a91caae
Jason Malan <jasondm1291@gmail.com>
2023-11-02 20:25:32 +0200

    Adding hooks for getting bookings and stays.

---
+++ 17-the-wild-oasis/the-wild-oasis/src/features/dashboard/useRecentBookings.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { subDays } from "date-fns";
+import { useSearchParams } from "react-router-dom";
+import { getBookingsAfterDate } from "../../services/apiBookings";
+
+export function useRecentBookings() {
+  const [searchParams] = useSearchParams();
+
+  const last = searchParams.get("last");
+  const numDays = last ? Number(last) : 7;
+  const queryDate = subDays(new Date(), numDays).toISOString();
+
+  const { isLoading, data: bookings } = useQuery({
+    queryFn: () => getBookingsAfterDate(queryDate),
+    queryKey: ["bookings", `last-${numDays}`],
+  });
+
+  return { isLoading, bookings };
+}
